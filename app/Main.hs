b2a8fa90b0e564f1d6d44c33d5abd9fe6b9eed70
Victor Smirnov <vityaman.dev@yandex.ru>
2026-03-14 15:41:25 +0300

    Introduce CLI and Lexer & Diagnostic dump (#8)
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main (main) where
 
-import Lib
+import qualified CLI
+import Control.Monad.Writer (runWriter)
+import qualified Diagnostic
+import qualified Lexer
 
 main :: IO ()
-main = someFunc
+main = do
+  CLI.Args
+    { CLI.inputPath = inputPath,
+      CLI.diagnosticsPath = diagnosticsPath,
+      CLI.tokensPath = tokensPath
+    } <-
+    CLI.parseArgs
+
+  content <- readFile inputPath
+
+  let (tokens, diagnostics) = runWriter $ Lexer.scan content
+
+  writeFile diagnosticsPath (Diagnostic.displays diagnostics)
+  writeFile tokensPath (Lexer.display tokens)
+
+  return ()
---
+++ src/Lexer.hs
@@ -0,0 +1,44 @@
+module Lexer
+  ( AlexToken,
+    StellaToken (StellaToken),
+    scan,
+    display,
+  )
+where
+
+import Control.Monad (foldM)
+import Control.Monad.Writer
+import Data.List (singleton)
+import Diagnostic (Diagnostic (Diagnostic), Diagnostics, Severity (Error))
+import Position (Position (Position), pointRange)
+import Syntax.LexStella (Token (Err, PT), tokenText)
+import Syntax.ParStella (myLexer)
+
+type AlexToken = Token
+
+newtype StellaToken = StellaToken AlexToken
+
+instance Show StellaToken where
+  show (StellaToken token@(PT position _)) =
+    ":" ++ show (Position position) ++ "\t" ++ tokenText token
+  show (StellaToken (Err _)) = undefined
+
+scan :: String -> Writer Diagnostics [StellaToken]
+scan input =
+  foldM visit [] $ myLexer input
+  where
+    visit' :: AlexToken -> Writer Diagnostics (Maybe StellaToken)
+    visit' alex = case alex of
+      (Err posn) -> do
+        tell [Diagnostic Error (pointRange $ Position posn) "illegal token"]
+        return Nothing
+      token ->
+        return $ Just $ StellaToken token
+
+    visit :: [StellaToken] -> AlexToken -> Writer Diagnostics [StellaToken]
+    visit acc alex = do
+      token' <- visit' alex
+      return $ acc ++ maybe [] singleton token'
+
+display :: [StellaToken] -> String
+display = unlines . map show
