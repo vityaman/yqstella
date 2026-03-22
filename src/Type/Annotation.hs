f1aef00a99d9414291776c695737cc06b73c5cd9
vityaman <vityaman.dev@yandex.ru>
2026-03-22 08:49:24 +0300

    Support Abstraction and Application
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- src/Type/Annotation.hs
+++ src/Type/Annotation.hs
@@ -7,12 +7,13 @@ import Control.Monad (unless, when)
 import Control.Monad.State
 import Control.Monad.Writer
 import Data.Foldable (find)
+import Data.Maybe (fromMaybe)
 import Diagnostic (Diagnostic (Diagnostic), Diagnostics, Severity (Error), notImplemented)
 import Position (Position, pointRange)
 import qualified Syntax.AbsStella as AST
 import Type.Context (Context)
 import qualified Type.Context as Context
-import Type.Core (Type)
+import Type.Core (Type (Type))
 import qualified Type.Core as Type
 
 type TypeAnnotationEnv a = WriterT Diagnostics (State Context) a
@@ -61,11 +62,7 @@ instance TypeAnnotatable AST.Decl' where
       (AST.NoThrowType _) -> pure ()
       (AST.SomeThrowType _ _) -> tell [notImplemented p "DeclFun ThrowType"]
 
-    let toPair (AST.AParamDecl _ (AST.StellaIdent key) t) = (key, Type.fromAST t)
-        withDecl paramdecl = let (key, t) = toPair paramdecl in Context.withTyped key t
-        extend paramdecls'' context = foldr withDecl context paramdecls''
-
-    expr' <- withStateTAE (extend paramdecls) (annotateType expr)
+    expr' <- withStateTAE (ctxExtendByParamDecls paramdecls) (annotateType expr)
     let (actualPosition, actualType) = annotation expr'
         argTypes = fmap (snd . toPair) paramdecls'
         type' = fmap (Type.fn argTypes) actualType
@@ -159,9 +156,15 @@ instance TypeAnnotatable AST.Expr' where
   annotateType x@(AST.TypeCast {}) = do
     tell [notImplemented (annotation x) "TypeCast"]
     return $ fmap (,Nothing) x
-  annotateType x@(AST.Abstraction {}) = do
-    tell [notImplemented (annotation x) "Abstraction"]
-    return $ fmap (,Nothing) x
+  annotateType (AST.Abstraction p paramdecls expr) = do
+    let paramdecls' = fmap (fmap (,Nothing)) paramdecls
+
+    expr' <- withStateTAE (ctxExtendByParamDecls paramdecls) (annotateType expr)
+    let (_, actualType) = annotation expr'
+        argTypes = fmap (snd . toPair) paramdecls'
+        type' = fmap (Type.fn argTypes) actualType
+
+    return $ AST.Abstraction (p, type') paramdecls' expr'
   annotateType x@(AST.Variant {}) = do
     tell [notImplemented (annotation x) "Variant"]
     return $ fmap (,Nothing) x
@@ -195,9 +198,36 @@ instance TypeAnnotatable AST.Expr' where
   annotateType x@(AST.Deref {}) = do
     tell [notImplemented (annotation x) "Deref"]
     return $ fmap (,Nothing) x
-  annotateType x@(AST.Application {}) = do
-    tell [notImplemented (annotation x) "Application"]
-    return $ fmap (,Nothing) x
+  annotateType (AST.Application p expr exprs) = do
+    expr' <- annotateType expr
+    exprs' <- mapM annotateType exprs
+
+    let (expr'Position, expr'Type) = annotation expr'
+
+    type' <- case expr'Type of
+      Just (Type (AST.TypeFun _ argTypes returnType)) -> do
+        let expectedArgTypes = fmap Type argTypes
+
+        tell $
+          [ mismatch p' e a
+            | ((p', Just a), e) <- zip (fmap annotation exprs') expectedArgTypes,
+              a /= e
+          ]
+
+        return $ Just $ Type returnType
+      Just actual -> do
+        let unknown = Type.fromAST' AST.TypeAuto
+            expectedArgTypes = fmap (fromMaybe unknown . snd . annotation) exprs'
+            expected = Type.fn expectedArgTypes unknown
+
+        let message = "type mismatch: expected " ++ show expected ++ ", got " ++ show actual
+        tell [Diagnostic Error (pointRange expr'Position) message]
+
+        return Nothing
+      Nothing ->
+        return Nothing
+
+    return $ AST.Application (p, type') expr' exprs'
   annotateType x@(AST.TypeApplication {}) = do
     tell [notImplemented (annotation x) "TypeApplication"]
     return $ fmap (,Nothing) x
@@ -297,14 +327,25 @@ instance TypeAnnotatable AST.Expr' where
     when (null type') $ tell [Context.unknownName p name]
     return $ AST.Var (p, type') stellaident
 
+toPair :: AST.ParamDecl' a -> (String, Type)
+toPair (AST.AParamDecl _ (AST.StellaIdent key) t) = (key, Type.fromAST t)
+
+ctxExtendByParamDecls :: (Foldable f) => f (AST.ParamDecl' a) -> Context -> Context
+ctxExtendByParamDecls paramdecls'' context = foldr withDecl context paramdecls''
+  where
+    withDecl paramdecl = let (key, t) = toPair paramdecl in Context.withTyped key t
+
 matchType :: Position -> (() -> AST.Type' ()) -> Maybe Type -> TypeAnnotationEnv (Maybe Type)
-matchType position expected actual =
-  case actual of
-    Just type' | type' == expected' -> pure $ pure type'
-    Just actual' -> tell [mismatch position expected' actual'] >> pure Nothing
-    Nothing -> pure Nothing
+matchType position expected = matchType' position expected'
   where
     expected' = Type.fromAST' expected
+
+matchType' :: Position -> Type -> Maybe Type -> TypeAnnotationEnv (Maybe Type)
+matchType' position expected actual =
+  case actual of
+    Just type' | type' == expected -> pure $ pure type'
+    Just actual' -> tell [mismatch position expected actual'] >> pure Nothing
+    Nothing -> pure Nothing
 
 mismatch :: Position -> Type -> Type -> Diagnostic
 mismatch position expected actual =
