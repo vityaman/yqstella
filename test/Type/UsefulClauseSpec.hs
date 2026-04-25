276c214b9abbd0deb358d6fad85020d832df1127
vityaman <vityaman.dev@yandex.ru>
2026-04-25 09:15:00 +0300

    Fix

--- test/Type/UsefulClauseSpec.hs
+++ test/Type/UsefulClauseSpec.hs
@@ -11,6 +11,8 @@ ctors "Bool" =
   Map.fromList [("True", []), ("False", [])]
 ctors "Maybe Bool" =
   Map.fromList [("Some", ["Bool"]), ("None", [])]
+ctors "Tuple<Nat, Bool>" =
+  Map.fromList [("tuple", ["Nat", "Bool"])]
 ctors "Variant<a: Unit, b: Bool>" =
   Map.fromList [("a", ["Unit"]), ("b", ["Bool"])]
 ctors "Unit" =
@@ -88,18 +90,19 @@ testExample5 =
 testExample6 :: TestTree
 testExample6 =
   testCase "Example6 - Exhaustive <| a, b : Bool |> (nullary a + b)" $
-    assertEqual "" Nothing actual
+    assertEqual "" expected actual
   where
     matrix =
       [ [C "a" [C "unit" [] ::: "Unit"] ::: "Variant<a: Unit, b: Bool>"],
         [C "b" [W ::: "Bool"] ::: "Variant<a: Unit, b: Bool>"]
       ]
     actual = i ctors ["Variant<a: Unit, b: Bool>"] matrix
+    expected = Nothing
 
 testExample7 :: TestTree
 testExample7 =
   testCase "Example7 - Nested sums: Nat + (Bool + (Nat -> Nat)) exhaustive" $
-    assertEqual "" Nothing actual
+    assertEqual "" expected actual
   where
     matrix =
       [ [C "inl" [W ::: "Nat"] ::: "Nat + (Bool + (Nat -> Nat))"],
@@ -107,6 +110,18 @@ testExample7 =
         [C "inr" [C "inr" [W ::: "Nat -> Nat"] ::: "Bool + (Nat -> Nat)"] ::: "Nat + (Bool + (Nat -> Nat))"]
       ]
     actual = i ctors ["Nat + (Bool + (Nat -> Nat))"] matrix
+    expected = Nothing
+
+testExample8 :: TestTree
+testExample8 =
+  testCase "Example8 - Tuple matching" $
+    assertEqual "" expected actual
+  where
+    matrix =
+      [ [C "tuple" [C "0" [] ::: "Nat", W ::: "Bool"] ::: "Tuple<Nat, Bool>"]
+      ]
+    actual = i ctors ["Tuple<Nat, Bool>"] matrix
+    expected = Just [C "tuple" [C "succ" [W ::: "Nat"] ::: "Nat",W ::: "Bool"] ::: "Tuple<Nat, Bool>"]
 
 main :: IO TestTree
 main =
@@ -119,5 +134,6 @@ main =
         testExample4,
         testExample5,
         testExample6,
-        testExample7
+        testExample7,
+        testExample8
       ]
