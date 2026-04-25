4befca6bd634d657ad683d666a9c659035475f76
vityaman <vityaman.dev@yandex.ru>
2026-04-25 14:04:17 +0300

    Fix type pretty print

--- src/Type/Core.hs
+++ src/Type/Core.hs
@@ -12,13 +12,79 @@ module Type.Core
 where
 
 import Control.Monad (void)
-import Syntax.PrettyPrint (displayAST)
+import Data.List (intercalate)
 import qualified SyntaxGen.AbsStella as AST
 
 newtype Type = Type (AST.Type' ()) deriving (Eq, Ord)
 
 instance Show Type where
-  show (Type x) = displayAST x
+  show (Type x) = go 0 x
+    where
+      prec :: AST.Type' () -> Int
+      prec AST.TypeFun {} = 0
+      prec AST.TypeForAll {} = 0
+      prec AST.TypeRec {} = 0
+      prec AST.TypeSum {} = 1
+      prec AST.TypeTuple {} = 2
+      prec AST.TypeRecord {} = 2
+      prec AST.TypeVariant {} = 2
+      prec AST.TypeList {} = 2
+      prec AST.TypeRef {} = 2
+      prec _ = 3
+
+      parensIf :: Bool -> String -> String
+      parensIf True s = "(" ++ s ++ ")"
+      parensIf False s = s
+
+      go :: Int -> AST.Type' () -> String
+      go ctx t = parensIf (prec t < ctx) rendered
+        where
+          rendered = case t of
+            AST.TypeAuto _ ->
+              "auto"
+            AST.TypeFun _ args ret ->
+              "fn(" ++ intercalate ", " (map (go 0) args) ++ ") -> " ++ go 0 ret
+            AST.TypeForAll _ idents body ->
+              "forall " ++ unwords (map prettyN idents) ++ ". " ++ go 0 body
+            AST.TypeRec _ ident body ->
+              "µ " ++ prettyN ident ++ ". " ++ go 0 body
+            AST.TypeSum _ lhs rhs ->
+              go 2 lhs ++ " + " ++ go 2 rhs
+            AST.TypeTuple _ ts ->
+              "{" ++ intercalate ", " (map (go 0) ts) ++ "}"
+            AST.TypeRecord _ fields ->
+              "{" ++ intercalate ", " (map prettyRF fields) ++ "}"
+            AST.TypeVariant _ fields ->
+              "<| " ++ intercalate ", " (map prettyVF fields) ++ " |>"
+            AST.TypeList _ item ->
+              "[" ++ go 0 item ++ "]"
+            AST.TypeRef _ t2 ->
+              "&" ++ go 2 t2
+            AST.TypeBool _ ->
+              "Bool"
+            AST.TypeNat _ ->
+              "Nat"
+            AST.TypeUnit _ ->
+              "Unit"
+            AST.TypeTop _ ->
+              "Top"
+            AST.TypeBottom _ ->
+              "Bot"
+            AST.TypeVar _ ident ->
+              prettyN ident
+
+      prettyN :: AST.StellaIdent -> String
+      prettyN (AST.StellaIdent s) = s
+
+      prettyRF :: AST.RecordFieldType' () -> String
+      prettyRF (AST.ARecordFieldType _ n t) =
+        prettyN n ++ " : " ++ go 0 t
+
+      prettyVF :: AST.VariantFieldType' () -> String
+      prettyVF (AST.AVariantFieldType _ n (AST.NoTyping _)) =
+        prettyN n
+      prettyVF (AST.AVariantFieldType _ n (AST.SomeTyping _ t')) =
+        prettyN n ++ " : " ++ go 0 t'
 
 fromAST :: AST.Type' a -> Type
 fromAST t = Type $ void t
