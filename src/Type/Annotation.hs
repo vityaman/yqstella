f49e36ae55a38026cf381758bcd8e0e7ed20be84
Victor Smirnov <vityaman.dev@yandex.ru>
2026-05-12 16:37:11 +0300

    Support `#references`, `#sequencing` (#102)

--- src/Type/Annotation.hs
+++ src/Type/Annotation.hs
@@ -22,6 +22,7 @@ import Type.Expectation (TypeKind (Expected, Inferred), liftType, liftType', lis
 import Type.Expression (annotateTT2B, annotateTT2T)
 import Type.Match (annotateLetType, annotateMatchType)
 import Type.Record (annotateDotRecordType, annotateRecordType)
+import Type.Reference (annotateRefExprType)
 import Type.Tuple (annotateDotTupleType, annotateTupleType)
 import Type.Variant (variantExprTyping, variantFieldTyping)
 
@@ -124,12 +125,10 @@ instance TypeAnnotatable AST.ExprData' where
     return (AST.NoExprData (p, t'))
 
 instance TypeAnnotatable AST.Expr' where
-  annotateType _ x@(AST.Sequence p _ _) = do
-    tell [notImplemented p "Sequence"]
-    return $ stub x
-  annotateType _ x@(AST.Assign {}) = do
-    tell [notImplemented (annotation x) "Assign"]
-    return $ stub x
+  annotateType t x@(AST.Sequence {}) = do
+    annotateRefExprType t x annotateType
+  annotateType t x@(AST.Assign {}) = do
+    annotateRefExprType t x annotateType
   annotateType t (AST.If p condition thenB elseB) = do
     condition' <- checkType (Type.fromAST' AST.TypeBool) condition
     thenB' <- annotateType t thenB
@@ -221,12 +220,10 @@ instance TypeAnnotatable AST.Expr' where
     rhs' <- checkType (Type.fromAST' AST.TypeBool) rhs
     t' <- liftType p AST.TypeBool t
     return (AST.LogicAnd (p, Just t') lhs' rhs')
-  annotateType _ x@(AST.Ref {}) = do
-    tell [notImplemented (annotation x) "Ref"]
-    return $ stub x
-  annotateType _ x@(AST.Deref {}) = do
-    tell [notImplemented (annotation x) "Deref"]
-    return $ stub x
+  annotateType t x@(AST.Ref {}) =
+    annotateRefExprType t x annotateType
+  annotateType t x@(AST.Deref {}) = do
+    annotateRefExprType t x annotateType
   annotateType t (AST.Application p f xs) =
     annotateApplicationType t p f xs annotateType
   annotateType _ x@(AST.TypeApplication {}) = do
@@ -380,9 +377,8 @@ instance TypeAnnotatable AST.Expr' where
           return Nothing
 
     return $ AST.ConstInt (p, t') n
-  annotateType _ x@(AST.ConstMemory {}) = do
-    tell [notImplemented (annotation x) "ConstMemory"]
-    return $ stub x
+  annotateType t x@(AST.ConstMemory {}) =
+    annotateRefExprType t x annotateType
   annotateType t (AST.Var p stellaident@(AST.StellaIdent name)) = do
     context <- get
 
---
+++ src/Type/Reference.hs
@@ -0,0 +1,76 @@
+module Type.Reference (annotateRefExprType) where
+
+import Control.Monad.Writer (tell)
+import Diagnostic.Code (Code (..))
+import Diagnostic.Core (Severity (Error), diagnostic)
+import Diagnostic.Position (Position, pointRange)
+import qualified SyntaxGen.AbsStella as AST
+import Type.Core (Type (..))
+import qualified Type.Core as Type
+import Type.Env (TypeAnnotationEnv, TypeAnnotator, typeOf)
+import Type.Expectation (liftType)
+
+annotateRefExprType ::
+  Maybe Type ->
+  AST.Expr' Position ->
+  TypeAnnotator AST.Expr' ->
+  TypeAnnotationEnv (AST.Expr' (Position, Maybe Type))
+annotateRefExprType t (AST.Sequence p prv nxt) annotateType = do
+  prv' <- annotateType (Just $ Type.fromAST' AST.TypeUnit) prv
+  nxt' <- annotateType t nxt
+  return (AST.Sequence (p, typeOf prv' >> typeOf nxt') prv' nxt')
+annotateRefExprType t (AST.Assign p dst src) annotateType = do
+  _ <- liftType p AST.TypeUnit t
+
+  dst' <- annotateType Nothing dst
+  src'expected't <- case typeOf dst' of
+    Just (Type (AST.TypeRef () t')) ->
+      return $ Just $ Type.fromAST t'
+    Just t' -> do
+      let message = "expected a reference, but got " ++ show t'
+      tell [diagnostic Error NOT_A_REFERENCE (pointRange p) message]
+      return Nothing
+    Nothing ->
+      return Nothing
+  src' <- annotateType src'expected't src
+
+  return (AST.Assign (p, typeOf src' >> typeOf dst' >> Just (Type $ AST.TypeUnit ())) dst' src')
+annotateRefExprType Nothing (AST.Ref p expr) annotateType = do
+  expr' <- annotateType Nothing expr
+  let t = Type . AST.TypeRef () . (\(Type x) -> x) <$> typeOf expr'
+  return (AST.Ref (p, t) expr')
+annotateRefExprType (Just t'@(Type (AST.TypeRef () t))) (AST.Ref p expr) annotateType = do
+  let expr't = Type.fromAST t
+  expr' <- annotateType (Just expr't) expr
+  return (AST.Ref (p, typeOf expr' >> Just t') expr')
+annotateRefExprType (Just t) (AST.Ref p expr) annotateType = do
+  expr' <- annotateType Nothing expr
+  let message = "expected reference type, but got " ++ show t
+  tell [diagnostic Error UNEXPECTED_REFERENCE (pointRange p) message]
+  return (AST.Ref (p, Nothing) expr')
+annotateRefExprType t (AST.Deref p expr) annotateType = do
+  let ref't = Type . AST.TypeRef () . (\(Type x) -> x) <$> t
+  expr' <- annotateType ref't expr
+
+  t' <- case typeOf expr' of
+    Just (Type (AST.TypeRef () t')) ->
+      return $ Just $ Type t'
+    Just t'@(Type _) -> do
+      let message = "expected a reference type, got " ++ show t'
+      tell [diagnostic Error NOT_A_REFERENCE (pointRange p) message]
+      return Nothing
+    Nothing ->
+      return Nothing
+
+  return (AST.Deref (p, t') expr')
+annotateRefExprType Nothing (AST.ConstMemory p addr) _ = do
+  let message = "type inference for memory address is not supported (use type ascriptions)"
+  tell [diagnostic Error AMBIGUOUS_REFERENCE_TYPE (pointRange p) message]
+  return (AST.ConstMemory (p, Nothing) addr)
+annotateRefExprType t@(Just (Type (AST.TypeRef () _))) (AST.ConstMemory p addr) _ = do
+  return (AST.ConstMemory (p, t) addr)
+annotateRefExprType (Just t) (AST.ConstMemory p addr) _ = do
+  let message = "expected " ++ show t ++ ", but got memory address"
+  tell [diagnostic Error UNEXPECTED_MEMORY_ADDRESS (pointRange p) message]
+  return (AST.ConstMemory (p, Nothing) addr)
+annotateRefExprType _ _ _ = error "Unexpected non-reference expression"
