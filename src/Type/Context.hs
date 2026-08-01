5d4bbce5d41d95f09ad73ef457562a260966bed9
vityaman <vityaman.dev@yandex.ru>
2026-08-01 15:36:28 +0300

    add context exception type

--- src/Type/Context.hs
+++ src/Type/Context.hs
@@ -3,8 +3,10 @@ module Type.Context
     empty,
     withTyped,
     withTypeAliased,
+    withExceptionType,
     typeOf,
     typeWithAlias,
+    exceptionType,
     unknownName,
   )
 where
@@ -19,25 +21,36 @@ import Type.Core (Type)
 newtype Binding = Binding Type
   deriving (Show)
 
-data Context = Context (Map String Binding) (Map String Type)
+data Context = Context
+  { contextBindings :: Map String Binding,
+    contextTypeAliases :: Map String Type,
+    contextExceptionType :: Maybe Type
+  }
   deriving (Show)
 
 empty :: Context
-empty = Context Map.empty Map.empty
+empty = Context Map.empty Map.empty Nothing
 
 withTyped :: String -> Type -> Context -> Context
-withTyped key t (Context bindings typeAliases) =
-  Context (Map.insert key (Binding t) bindings) typeAliases
+withTyped key t (Context bindings typeAliases exceptionT) =
+  Context (Map.insert key (Binding t) bindings) typeAliases exceptionT
 
 withTypeAliased :: String -> Type -> Context -> Context
-withTypeAliased key t (Context bindings typeAliases) =
-  Context bindings (Map.insert key t typeAliases)
+withTypeAliased key t (Context bindings typeAliases exceptionT) =
+  Context bindings (Map.insert key t typeAliases) exceptionT
+
+withExceptionType :: Type -> Context -> Context
+withExceptionType t (Context bindings typeAliases _) =
+  Context bindings typeAliases (Just t)
 
 typeOf :: String -> Context -> Maybe Type
-typeOf key (Context bindings _) = (\(Binding x) -> x) <$> Map.lookup key bindings
+typeOf key ctx = (\(Binding x) -> x) <$> Map.lookup key (contextBindings ctx)
 
 typeWithAlias :: String -> Context -> Maybe Type
-typeWithAlias key (Context _ typeAliases) = Map.lookup key typeAliases
+typeWithAlias key ctx = Map.lookup key (contextTypeAliases ctx)
+
+exceptionType :: Context -> Maybe Type
+exceptionType = contextExceptionType
 
 unknownName :: Position -> String -> Diagnostic
 unknownName position name =
