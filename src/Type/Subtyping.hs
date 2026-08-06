8a0ab77199c1c946ddd4e7f56b6c7bdd52c7bd98
vityaman <vityaman.dev@yandex.ru>
2026-08-06 16:33:20 +0300

    variant

--- src/Type/Subtyping.hs
+++ src/Type/Subtyping.hs
@@ -3,8 +3,9 @@ module Type.Subtyping (liftSubType, liftSubType') where
 import Control.Monad.Writer (tell)
 import Data.Foldable (find)
 import Diagnostic.Code (Code (UNEXPECTED_SUBTYPE, UNEXPECTED_TYPE_FOR_EXPRESSION))
-import Diagnostic.Core (Diagnostic (range), Severity (Error), diagnostic)
+import Diagnostic.Core as Diagnostic
 import Diagnostic.Position (Position, pointRange, unknown)
+import Syntax.PrettyPrint (displayAST)
 import qualified SyntaxGen.AbsStella as AST
 import Type.Core (Type (Type))
 import Type.Env
@@ -36,12 +37,39 @@ subsumes lhsT'@(Type (AST.TypeRecord () lhs)) rhsT'@(Type (AST.TypeRecord () rhs
     subsumesF (AST.ARecordFieldType () (AST.StellaIdent rhsName) rhsT) lhs' =
       case find (\lhsF -> rhsName == nameOf' lhsF) lhs' of
         Nothing ->
-          let message =
+          let message' =
                 ("(subsumes) missing record field: " ++ rhsName ++ ", ")
                   ++ ("checking " ++ show lhsT' ++ " <: " ++ show rhsT')
-           in Left $ diagnostic Error UNEXPECTED_SUBTYPE (pointRange unknown) message
+           in Left $ diagnostic Error UNEXPECTED_SUBTYPE (pointRange unknown) message'
         Just lhsF ->
           let lhsT = typeOf' lhsF
-           in subsumes (Type lhsT) (Type rhsT)
+           in Type lhsT `subsumes` Type rhsT
+subsumes lhsT'@(Type (AST.TypeVariant () lhs)) rhsT'@(Type (AST.TypeVariant () rhs)) =
+  mapM_ (`subsumesF` rhs) lhs
+  where
+    nameOf' (AST.AVariantFieldType () (AST.StellaIdent x) _) = x
+    typingOf' (AST.AVariantFieldType () _ t) = t
+
+    subsumesF :: AST.VariantFieldType' () -> [AST.VariantFieldType' ()] -> Either Diagnostic ()
+    subsumesF (AST.AVariantFieldType () (AST.StellaIdent rhsName) rhsT) lhs' =
+      case find (\lhsF -> rhsName == nameOf' lhsF) lhs' of
+        Nothing ->
+          let message' =
+                ("(subsumes) unexpected variant field: " ++ rhsName ++ ", ")
+                  ++ ("checking " ++ show lhsT' ++ " <: " ++ show rhsT')
+           in Left $ diagnostic Error UNEXPECTED_SUBTYPE (pointRange unknown) message'
+        Just lhsF ->
+          let lhsT = typingOf' lhsF
+           in case (lhsT, rhsT) of
+                (AST.SomeTyping () lhsT'', AST.SomeTyping () rhsT'') ->
+                  Type lhsT'' `subsumes` Type rhsT''
+                (AST.NoTyping (), AST.NoTyping ()) ->
+                  return ()
+                (lhsT'', rhsT'') ->
+                  let message' =
+                        ("(subsumes) variant field " ++ show rhsName ++ " type mismatch: ")
+                          ++ (displayAST lhsT'' ++ " vs " ++ displayAST rhsT'')
+                   in Left $ diagnostic Error UNEXPECTED_SUBTYPE (pointRange unknown) message'
 subsumes lhs rhs =
-  Left $ mismatch UNEXPECTED_TYPE_FOR_EXPRESSION unknown lhs rhs
+  let d = mismatch UNEXPECTED_TYPE_FOR_EXPRESSION unknown lhs rhs
+   in Left d {message = "(subsumes) " ++ message d}
